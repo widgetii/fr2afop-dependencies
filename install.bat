1aad235f6223392ecdc72bf57998c285de264efe
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2013-07-07 21:29:17 +0300

    project added to git control

---
+++ install.bat
@@ -0,0 +1,6 @@
+CALL mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j-fop-ext -Dversion=2.1 -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j-fop-ext.jar
+
+CALL mvn install:install-file -DgroupId=org.firebirdsql.jdbc -DartifactId=jaybird-full -Dversion=2.2.3 -Dpackaging=jar -DgeneratePom=true -Dfile=jaybird-full-2.2.3.jar
+
+ECHO.
+PAUSE
