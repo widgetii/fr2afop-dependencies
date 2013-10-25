16fd97c640670dcbea59bde8d799a08b0d53937e
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2013-10-25 22:57:50 +0300

    added latest version of fop

--- install.bat
+++ install.bat
@@ -1,3 +1,7 @@
+CALL mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=fop -Dversion=svn-trunk -Dpackaging=jar -DgeneratePom=true -Dfile=fop-svn-trunk.jar
+
+CALL mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=xmlgraphics-commons -Dversion=svn-trunk -Dpackaging=jar -DgeneratePom=true -Dfile=xmlgraphics-commons-svn-trunk.jar
+
 CALL mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j-fop-ext -Dversion=2.1 -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j-fop-ext.jar
 
 CALL mvn install:install-file -DgroupId=org.firebirdsql.jdbc -DartifactId=jaybird-full -Dversion=2.2.3 -Dpackaging=jar -DgeneratePom=true -Dfile=jaybird-full-2.2.3.jar
--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=fop -Dversion=svn-trunk -Dpackaging=jar -DgeneratePom=true -Dfile=fop-svn-trunk.jar
+
+mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=xmlgraphics-commons -Dversion=svn-trunk -Dpackaging=jar -DgeneratePom=true -Dfile=xmlgraphics-commons-svn-trunk.jar
+
 mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j-fop-ext -Dversion=2.1 -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j-fop-ext.jar
 
 mvn install:install-file -DgroupId=org.firebirdsql.jdbc -DartifactId=jaybird-full -Dversion=2.2.3 -Dpackaging=jar -DgeneratePom=true -Dfile=jaybird-full-2.2.3.jar
