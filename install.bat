f785659d057caa69170e4ec1174a3ad005fdd873
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2013-11-28 02:29:35 +0300

    barcode4j latest added

--- install.bat
+++ install.bat
@@ -2,7 +2,9 @@ CALL mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=fop 
 
 CALL mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=xmlgraphics-commons -Dversion=svn-trunk -Dpackaging=jar -DgeneratePom=true -Dfile=xmlgraphics-commons-svn-trunk.jar
 
-CALL mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j-fop-ext -Dversion=2.1 -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j-fop-ext.jar
+CALL mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j -Dversion=latest -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j.jar
+
+CALL mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j-fop-ext -Dversion=latest -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j-fop-ext.jar
 
 CALL mvn install:install-file -DgroupId=org.firebirdsql.jdbc -DartifactId=jaybird-full -Dversion=2.2.3 -Dpackaging=jar -DgeneratePom=true -Dfile=jaybird-full-2.2.3.jar
 
--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=fop -Dver
 
 mvn install:install-file -DgroupId=org.apache.xmlgraphics -DartifactId=xmlgraphics-commons -Dversion=svn-trunk -Dpackaging=jar -DgeneratePom=true -Dfile=xmlgraphics-commons-svn-trunk.jar
 
-mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j-fop-ext -Dversion=2.1 -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j-fop-ext.jar
+mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j -Dversion=latest -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j.jar
+
+mvn install:install-file -DgroupId=net.sf.barcode4j -DartifactId=barcode4j-fop-ext -Dversion=latest -Dpackaging=jar -DgeneratePom=true -Dfile=barcode4j-fop-ext.jar
 
 mvn install:install-file -DgroupId=org.firebirdsql.jdbc -DartifactId=jaybird-full -Dversion=2.2.3 -Dpackaging=jar -DgeneratePom=true -Dfile=jaybird-full-2.2.3.jar
 
